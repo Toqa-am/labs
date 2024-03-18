095e24a9fbe1d3ee8f696004b751ee4229644010
Toqa-am <155821388+Toqa-am@users.noreply.github.com>
2024-03-19 01:32:01 +0200

    Add files via upload

---
+++ Java/day4/packs/throwEx.java
@@ -0,0 +1,15 @@
+package packs;
+
+public class throwEx {
+    public void firstMeth() throws exep {
+        throw new exep("Exception from first method");
+    }
+
+    public void secondMeth() throws exep {
+        throw new exep("Exception from second method");
+    }
+
+    public void thirdMeth() throws exep {
+        throw new exep("Exception from third method");
+    }
+}
